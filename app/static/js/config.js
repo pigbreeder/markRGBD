0d764218f4bed20ae555baf28beb3082e6c0f2b1
pigbreeder <pigbreeder@foxmail.com>
2018-01-16 11:19:39 +0800

    modify selected mark color

--- app/static/js/config.js
+++ app/static/js/config.js
@@ -4,4 +4,4 @@ var HEIGHT_OFFSET = 100;
 var SWITCH_SHOW_MARK_IDX = false;
 var COLOR_MARK = 'red';
 var COLOR_MARK_IDX = '#28ff28';
-var COLOR_LIGHT_MARK = 'green';
+var COLOR_LIGHT_MARK = '#F9F900';
