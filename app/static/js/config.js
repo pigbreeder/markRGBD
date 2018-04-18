bcb596066a56e093446e2d553b4e5b6179148363
pigbreeder <pigbreeder@foxmail.com>
2018-04-18 11:04:05 +0800

    增加修正标记功能

--- app/static/js/config.js
+++ app/static/js/config.js
@@ -2,6 +2,7 @@ var CIRCLE_IDX_LOC = 0;
 var WIDTH_OFFSET = 20;
 var HEIGHT_OFFSET = 100;
 var SWITCH_SHOW_MARK_IDX = true;
+var SWITCH_MODIFY_MARK = false;
 var COLOR_MARK = 'red';
 var COLOR_MARK_IDX = '#28ff28';
 var COLOR_LIGHT_MARK = '#F9F900';
